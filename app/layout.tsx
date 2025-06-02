c3e3aeb4badb42ced7cae6ccbd7fa6d6bbeb7352
Shibuya14 <KeigoRenraku14@gmail.com>
2025-06-02 12:49:11 +0900

    fix title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: '渋谷佳吾',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
