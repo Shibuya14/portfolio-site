99ef2db92703fba07040af1b71a30fff39289aa9
Shibuya14 <KeigoRenraku14@gmail.com>
2025-07-06 15:26:46 +0900

    アイコン追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ export const metadata: Metadata = {
   title: '渋谷佳吾',
   description: 'Created with v0',
   generator: 'v0.dev',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/apple-icon.png',
+  },
 }
 
 export default function RootLayout({
